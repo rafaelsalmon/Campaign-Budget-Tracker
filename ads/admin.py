7dab4eb27d1e4a7dbb94760a6520244cb19d13f0
Rafael Salmon <rafael@codifique.com.br>
2025-06-24 12:56:23 -0300

    Update admin.py
    Testing

--- ads/admin.py
+++ ads/admin.py
@@ -2,20 +2,18 @@ from django.contrib import admin
 from .models import Brand, Campaign, Schedule
 
 
-@admin.register(Brand)
-class BrandAdmin(admin.ModelAdmin):
-    list_display = ("name", "daily_budget", "monthly_budget")
-    search_fields = ("name",)
+# @admin.register(Brand)
+# class BrandAdmin(admin.ModelAdmin):
+#     list_display = ("name", "daily_budget", "monthly_budget")
+#     search_fields = ("name",)
 
+# @admin.register(Campaign)
+# class CampaignAdmin(admin.ModelAdmin):
+#     list_display = ("name", "brand", "is_active", "created_at")
+#     list_filter = ("is_active", "brand")
+#     search_fields = ("name",)
 
-@admin.register(Campaign)
-class CampaignAdmin(admin.ModelAdmin):
-    list_display = ("name", "brand", "is_active", "created_at")
-    list_filter = ("is_active", "brand")
-    search_fields = ("name",)
-
-
-@admin.register(Schedule)
-class ScheduleAdmin(admin.ModelAdmin):
-    list_display = ("start_hour", "end_hour")
-    list_filter = ("start_hour", "end_hour")
+# @admin.register(Schedule)
+# class ScheduleAdmin(admin.ModelAdmin):
+#     list_display = ("start_hour", "end_hour")
+#     list_filter = ("start_hour", "end_hour")
